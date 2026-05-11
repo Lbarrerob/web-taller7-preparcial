d0296c59e02cbb7db6403eb275cd9f88423cb72b
Lbarrerob <l.barrerob@uniandes.edu.co>
2026-05-10 20:19:51 -0500

    bank

--- src/bank/bank.module.ts
+++ src/bank/bank.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Account } from './entities/account.entity';
+import { Loan } from './entities/loan.entity';
 import { BankService } from './bank.service';
 import { BankController } from './bank.controller';
+import { AuthModule } from '../auth/auth.module';
+
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Account, Loan]), // registra los repos
+    AuthModule,                                    // para el guard
+  ],
   controllers: [BankController],
   providers: [BankService],
 })
